514d8c3c604dae0069f3d45430eee7c27bbee7b7
topcat <bfonseca@ipt.br>
2023-11-08 10:26:46 -0300

    Removed intermediate test folder.

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-
-mkdir test
-cd test
 svn checkout https://github.com/iptsp/anchor-kernels/trunk/$1
 
-ls | awk '! /dbg/ {print $1"/"$0}' | dpkg -i 
-
-rm -rf test
+ls $1 | awk '! /dbg/ {print $1"/"$0}' | dpkg -i 
