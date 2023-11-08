9a94f5263b2f823af517da17589bdcdd9866045a
topcat <bfonseca@ipt.br>
2023-11-08 10:23:44 -0300

    Fixed '-v'.

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ mkdir test
 cd test
 svn checkout https://github.com/iptsp/anchor-kernels/trunk/$1
 
-ls | awk -v '! /dbg/ {print $1"/"$0}' | dpkg -i 
+ls | awk '! /dbg/ {print $1"/"$0}' | dpkg -i 
 
 rm -rf test
